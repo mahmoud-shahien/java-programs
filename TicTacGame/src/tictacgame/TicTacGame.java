4248c433804a23360942582cf431bac188db8aac
mahmoud-shahien <79414875+mahmoud-shahien@users.noreply.github.com>
2021-02-21 19:07:18 +0200

    Add files via upload

---
+++ TicTacGame/src/tictacgame/TicTacGame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictacgame;
+
+/**
+ *
+ * @author convert
+ */
+public class TicTacGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmMain main =new frmMain();
+        main.setLocationRelativeTo(null);
+        main.setVisible(true);
+       
+    }
+    
+}
