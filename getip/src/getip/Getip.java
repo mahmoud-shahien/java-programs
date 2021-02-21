9aeaedb91938c100b1206d48f26a68d5fe296242
mahmoud-shahien <79414875+mahmoud-shahien@users.noreply.github.com>
2021-02-21 19:12:30 +0200

    Add files via upload

---
+++ getip/src/getip/Getip.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getip;
+
+/**
+ *
+ * @author convert
+ */
+public class Getip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        get g = new get();
+        g.setVisible(true);
+    }
+    
+}
